f50f9cb462ce9540f0859322946682dc65aaab88
Ucok23 <ulubis98@gmail.com>
2025-03-08 12:09:27 +0700

    fix parser to only do parsing

--- src/siesvi.parser.ts
+++ src/siesvi.parser.ts
@@ -3,67 +3,88 @@ import { Transform, TransformCallback } from "stream";
 interface CsvParserConfig<T> {
     delimiter: string;
     headers?: string[];
-    validate?: (row: T) => boolean;
-    filter?: (row: T) => boolean;
-    transform?: (row: T) => T;
 }
 
-class CsvParser<T> extends Transform {
+type AllowedType = string | number | boolean | Date
+
+class CsvParser<T extends Record<string, AllowedType>> extends Transform {
     private config: CsvParserConfig<T>;
 
-    constructor(config: CsvParserConfig<T> = {delimiter: ','}) {
-        super({objectMode: true});
+    constructor(config: CsvParserConfig<T> = { delimiter: "," }) {
+        super({ objectMode: true });
         this.config = config;
     }
 
-    _transform(chunk: Buffer, encoding: BufferEncoding, callback: TransformCallback) {
+    _transform(
+        chunk: Buffer,
+        encoding: BufferEncoding,
+        callback: TransformCallback
+    ) {
         try {
-
             const lines = chunk.toString(encoding).split("\n");
 
-            if (this.config.headers === null || this.config.headers === undefined) {
-                if (lines.length > 0) {
-                    this.config.headers = lines[0].split(this.config.delimiter);
+            if (this.config.headers === undefined) {
+                if (lines.length > 0 && lines[0].trim() !== "") {
+                    this.config.headers = lines[0].split(this.config.delimiter).map(header => header.trim());
                     lines.shift(); // Remove the header line from the data lines
+                } else {
+                    //if first line is empty, skip and wait for next chunk, or if no data, just return
+                    if (lines.length === 1 && lines[0].trim() === "") {
+                        return callback(null);
+                    }
+                    if (lines.length === 0) {
+                        return callback(null);
+                    }
                 }
             }
 
             if (this.config.headers) {
                 // Process data rows
                 for (const line of lines) {
-                    const data = line.split(this.config.delimiter);
-                    if (data.length === this.config.headers.length && data[0] !== "") {
-                        let obj: any = {};
-                        for (let j = 0; j < this.config.headers.length; j++) {
-                            obj[this.config.headers[j]] = data[j];
-                        }
-
-                        if (this.config.validate && !this.config.validate(obj)) {
-                            continue;
-                        }
-
-                        if (this.config.filter && !this.config.filter(obj)) {
-                            continue;
-                        }
-
-                        if (this.config.transform) {
-                            obj = this.config.transform(obj)
-                        }
-
-                        this.push(obj);
+                    if (line.trim() === "") {
+                        continue; // Skip empty lines
                     }
-                    else {
-                       throw Error('Malformed Csv File')
+                    const data = line.split(this.config.delimiter).map(item => item.trim());
+                    let obj: Record<string, AllowedType> = {};
+                    for (let j = 0; j < this.config.headers.length; j++) {
+                        const key = this.config.headers[j]
+                        const value = data[j]
+
+                        if (!isNaN(Number(value))) {
+                            if (value.includes(".")) {
+                                obj[key] = parseFloat(value);
+                            } else {
+                                obj[key] = parseInt(value, 10);
+                            }
+                        }
+                        else if (value === 'true') {
+                            obj[key] = true
+                        }
+                        else if (value === 'false') {
+                            obj[key] = false
+                        }
+                        else {
+                            try {
+                                obj[key] = new Date(value);
+                                if (isNaN(obj[key].getTime())) {
+                                    obj[key] = value;
+                                }
+
+                            } catch (e) {
+                                obj[key] = value;
+                            }
+                        }
                     }
+                    this.push(obj);
                 }
             }
 
             callback(null);
-        }
-        catch (error: any) {
+        } catch (error: any) {
             callback(error);
         }
     }
 }
+
 
 export { CsvParser, CsvParserConfig };
